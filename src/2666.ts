c0819f3c36ccee90ea5d4d08c145caa26a9749fa
SvZ <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-03-12 10:31:34 +0100

    Add solution 2666

---
+++ src/2666.ts
@@ -0,0 +1,29 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let hasBeenCalled = false;
+
+  return function (...args) {
+    if (hasBeenCalled) {
+      return undefined;
+    }
+
+    hasBeenCalled = true;
+    return fn(...args);
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
